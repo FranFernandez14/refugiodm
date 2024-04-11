d5c863e8d9ced38ca0432bc913e10b3fe186424e
FranFernandez14 <fran.joa.fernandez@gmail.com>
2024-04-11 20:25:58 -0300

    reservas fixed

--- front/src/components/admin/containers/ReservaContainer.jsx
+++ front/src/components/admin/containers/ReservaContainer.jsx
@@ -7,21 +7,23 @@ import { Link } from 'react-router-dom';
 
 const ReservaContainer = () => {
   const [reservas, setReservas] = useState([]);
-  const [estadoReserva, setEstadoReserva] = useState(5);
+  const [estadoReserva, setEstadoReserva] = useState(1);
   const [fechaFinal, setFechaFinal] = useState('');
   const [fechaInicio, setFechaInicio] = useState('');
   const [currentPage, setCurrentPage] = useState(0);
 
   const fetchReservasByState = async (estado, fechaInicio, fechaFin, page) => {
     try {
+      let queryParams = { estado: estado, page: page, size: 10 };
+      if (fechaInicio) {
+        queryParams.fechaInicio = fechaInicio;
+      }
+      if (fechaFin) {
+        queryParams.fechaFin = fechaFin;
+      }
+  
       const response = await axios.get('http://localhost:8080/api/reservas/byState', {
-        params: {
-          estado: estado,
-          fechaInicio: fechaInicio,
-          fechaFin: fechaFin,
-          page: page,
-          size: 10
-        },
+        params: queryParams,
         headers: {
           Authorization: `Bearer ${localStorage.getItem('token')}`
         }
@@ -31,12 +33,10 @@ const ReservaContainer = () => {
     } catch (error) {
       console.error('Error fetching reservas by state:', error);
     }
-};
-
-  
+  };
 
   useEffect(() => {
-    fetchReservasByState("", "", "", currentPage); // Cargar inicialmente las reservas de la página 1
+    fetchReservasByState(1, "", "", currentPage); // Cargar inicialmente las reservas pendientes
   }, [currentPage]); // Ejecutar cuando currentPage cambia
 
   const goToPreviousPage = () => {
@@ -49,35 +49,39 @@ const ReservaContainer = () => {
     setCurrentPage(currentPage + 1); // Ir a la página siguiente
   };
 
+  const handleDateFilterChange = () => {
+    fetchReservasByState(estadoReserva, fechaInicio, fechaFinal, currentPage); // Actualizar reservas con las fechas y estado actual
+  };
+
   return (
     <div className='admin-container'>
       <Sidebar />
       <div className='admin-right-content'>
         <div id='reserva-buttons'>
           <div>
-            <Link onClick={() => fetchReservasByState(3, fechaInicio, fechaFinal, currentPage)}>Ver reservas pendientes</Link>
+            <Link onClick={() => {setCurrentPage(0); setEstadoReserva(1); fetchReservasByState(1, "", "", 0);}}>Ver reservas pendientes</Link>
           </div>
           <div>
-            <Link onClick={() => fetchReservasByState(1, fechaInicio, fechaFinal, currentPage)}>Ver reservas canceladas</Link>
+            <Link onClick={() => {setCurrentPage(0); setEstadoReserva(2); fetchReservasByState(2, "", "", 0);}}>Ver reservas canceladas</Link>
           </div>
           <div>
-            <Link onClick={() => fetchReservasByState(4, fechaInicio, fechaFinal, currentPage)}>Ver reservas aceptadas</Link>
+            <Link onClick={() => {setCurrentPage(0); setEstadoReserva(3); fetchReservasByState(3, "", "", 0);}}>Ver reservas aceptadas</Link>
           </div>
           <div>
-            <Link onClick={() => fetchReservasByState(7, fechaInicio, fechaFinal, currentPage)}>Ver reservas iniciadas</Link>
+            <Link onClick={() => {setCurrentPage(0); setEstadoReserva(4); fetchReservasByState(4, "", "", 0);}}>Ver reservas iniciadas</Link>
           </div>
           <div>
-            <Link onClick={() => fetchReservasByState(6, fechaInicio, fechaFinal, currentPage)}>Ver reservas finalizadas</Link>
+            <Link onClick={() => {setCurrentPage(0); setEstadoReserva(5); fetchReservasByState(5, "", "", 0);}}>Ver reservas finalizadas</Link>
           </div>
         </div>
         <div className='filtroFecha'>
           <div className='Filtro'>
             <label htmlFor="FechaInicio">Fecha de Inicio:</label>
-            <input type="date" id="FechaInicio" value={fechaInicio} onChange={(e) => setFechaInicio(e.target.value)} />
+            <input type="date" id="FechaInicio" value={fechaInicio} onChange={(e) => setFechaInicio(e.target.value)} onBlur={handleDateFilterChange} />
           </div>
           <div className='Filtro'>
             <label htmlFor="FechaFin">Fecha de Fin:</label>
-            <input type="date" id="FechaFin" value={fechaFinal} onChange={(e) => setFechaFinal(e.target.value)} />
+            <input type="date" id="FechaFin" value={fechaFinal} onChange={(e) => setFechaFinal(e.target.value)} onBlur={handleDateFilterChange} />
           </div>
         </div>
         <Reserva reservas={reservas} estadoReserva={estadoReserva} />
